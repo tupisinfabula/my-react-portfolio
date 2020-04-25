89a6f4e42bf4834eb276ffa2b3ff31c3cc570db3
tupisinfabula <seila.tupone@gmail.com>
2020-04-25 23:21:55 +0200

    Create components and data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import $ from 'jquery';
 import './App.css';
+import Header from './Components/Header';
+import About from './Components/About';
+import Resume from './Components/Resume';
+import LifeStyle from './Components/LifeStyle';
+import Portfolio from './Components/Portfolio';
+import Footer from './Components/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    getResumeData() {
+        $.ajax({
+            url: '/resumeData.json',
+            dataType: 'json',
+            cache: false,
+            success: function (data) {
+                this.setState({resumeData: data});
+            }.bind(this),
+            error: function (xhr, status, err) {
+                console.log(err);
+            }
+        });
+    }
+
+    componentDidMount() {
+        this.getResumeData();
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Header data={this.state.resumeData.main}/>
+                <About data={this.state.resumeData.main}/>
+                <Resume data={this.state.resumeData.resume}/>
+                <LifeStyle data={this.state.resumeData.lifeStyle}/>
+                <Portfolio data={this.state.resumeData.portfolio}/>
+                <Footer data={this.state.resumeData.main}/>
+            </div>
+        );
+    }
 }
 
 export default App;
