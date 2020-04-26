afab136d22d529025eeb653d4844d90ea797659e
tupisinfabula <seila.tupone@gmail.com>
2020-04-26 12:17:34 +0200

    Fix url resumeData

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
 
     getResumeData() {
         $.ajax({
-            url: '/resumeData.json',
+            url: './resumeData.json',
             dataType: 'json',
             cache: false,
             success: function (data) {
