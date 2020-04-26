935575fd72fb99ada9bc90d9425b269ef98f76c5
tupisinfabula <seila.tupone@gmail.com>
2020-04-26 09:27:57 +0200

    Updates

---
+++ precache-manifest.2713466ed2ad2098b67ff67249876fe2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da1a7ef5915215c5f7e930675e28085f",
+    "url": "/my-react-portfolio/index.html"
+  },
+  {
+    "revision": "5e14a9105ff1e47a68e7",
+    "url": "/my-react-portfolio/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "64ca2a2fa18b0ca6ac02",
+    "url": "/my-react-portfolio/static/js/2.e63447fc.chunk.js"
+  },
+  {
+    "revision": "6e742f513d9938d07b2d93218f3e4449",
+    "url": "/my-react-portfolio/static/js/2.e63447fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e14a9105ff1e47a68e7",
+    "url": "/my-react-portfolio/static/js/main.aaca5ef9.chunk.js"
+  },
+  {
+    "revision": "6b757e07b1141854b7f8",
+    "url": "/my-react-portfolio/static/js/runtime-main.57e78bfe.js"
+  }
+]);
